a3721143516d341e4c42a69c440bac818aee123d
Akshay200228 <sankpla2812@gmail.com>
2023-06-27 20:34:40 +0530

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { navLinks } from '../constants';
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const [logoAnimation, setLogoAnimation] = useState(true);
-  const [showButtons, setShowButtons] = useState(false);
+  const [animateButton, setAnimateButton] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -15,23 +15,26 @@ const Navbar = () => {
   }, []);
 
   useEffect(() => {
-    setShowButtons(false); // Hide the buttons initially when the menu is closed
-  }, [toggle]);
+    const interval = setInterval(() => {
+      setAnimateButton((prevAnimateButton) => !prevAnimateButton);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <nav className='fixed top-0 z-50 flex items-center justify-between w-full py-4 navbar backdrop-filter backdrop-blur-lg bg-opacity-70'>
+    <nav className="fixed top-0 z-50 flex items-center justify-between w-full py-4 navbar backdrop-filter backdrop-blur-lg bg-opacity-70">
       <img
         src={logo6}
         alt="logo"
         className={`w-[124px] h-[32px] ${logoAnimation ? 'animate-spin' : ''}`}
       />
-      <ul className='items-center justify-end hidden space-x-4 sm:flex'>
+      <ul className="items-center justify-end hidden space-x-4 sm:flex">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${
               index === navLinks.length - 1 ? 'mr-0' : 'mr-0'
-            } text-white hover:text-sky-200`}
+            } text-white hover:text-purple-400`}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
@@ -40,7 +43,9 @@ const Navbar = () => {
       <div className="hidden mr-20 space-x-4 sm:flex">
         <button
           type="button"
-          className="animate-pulse py-4 px-6 font-poppins font-medium text-[14px] text-primary bg-purple-gradient rounded-[10px] outline-none"
+          className={`py-4 px-6 font-poppins font-medium text-[14px] text-primary bg-purple-gradient rounded-[10px] outline-none ${
+            animateButton ? 'animate-pulse' : ''
+          }`}
         >
           Get Started
         </button>
@@ -50,22 +55,18 @@ const Navbar = () => {
       </div>
 
       {/* Mobile Navbar */}
-
-      <div className='flex items-center justify-end flex-1 mr-14 sm:hidden'>
+      <div className="flex items-center justify-end flex-1 mr-14 sm:hidden">
         <img
           src={toggle ? close : menu}
           alt="menu"
-          className='w-[28px] h-[28px] object-contain'
+          className="w-[28px] h-[28px] object-contain"
           onClick={() => {
-            setToggle(!toggle);
-            setShowButtons(false); // Hide the buttons when toggling the menu
+            setToggle((prevToggle) => !prevToggle);
           }}
         />
 
         {toggle && (
-          <div
-            className="mr-10 flex flex-col items-center p-6 bg-black-gradient absolute top-full right-0 mt-2 mx-4 min-w-[140px] rounded-xl sidebar"
-          >
+          <div className="mr-10 flex flex-col items-center p-6 bg-black-gradient absolute top-full right-0 mt-2 mx-4 min-w-[140px] rounded-xl sidebar">
             <ul className="flex flex-col items-start justify-end flex-1 mb-4 list-none">
               {navLinks.map((nav, index) => (
                 <li
