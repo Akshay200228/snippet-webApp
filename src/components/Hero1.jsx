a2f6c0a8fb54b8f362cac668ec4fd0d8e888be6b
Akshay200228 <sankpla2812@gmail.com>
2023-06-29 12:36:54 +0530

    performance optimizations1

--- src/components/Hero1.jsx
+++ src/components/Hero1.jsx
@@ -90,11 +90,11 @@ const Hero1 = () => {
 
           {/* Gradients */}
           {/* Gradient 1 */}
-          <div className="absolute z-0 w-[40%] h-[35%] top-0 purple__gradient" />
+          {/* <div className="absolute z-0 w-[40%] h-[35%] top-0 purple__gradient" /> */}
           {/* Gradient 2 */}
-          <div className="absolute z-1 w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+          {/* <div className="absolute z-1 w-[80%] h-[80%] rounded-full white__gradient bottom-40" /> */}
           {/* Gradient 3 */}
-          <div className="absolute z-0 w-[50%] h-[50%] right-20 bottom-20 purple__gradient" />
+          {/* <div className="absolute z-0 w-[50%] h-[50%] right-20 bottom-20 purple__gradient" /> */}
         </div>
       </div>
     </section>
