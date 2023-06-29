a67ef5eac598042c0a75f1bdcb8b54f361cf4e1a
Akshay200228 <sankpla2812@gmail.com>
2023-06-29 13:20:40 +0530

    performance optimizations7

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,7 @@
-import bill from "./bill.png";
 import card from "./card.png";
-import logo from "./logo.svg";
 import logo6 from "./logo6.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import arrowUp from "./arrow-up.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
@@ -13,17 +10,13 @@ import model3 from "./model3.png";
 import model4 from "./model4.png";
 import Object_cal from "./Object_cal.png";
 import phone from "./phone.png";
-import headphones from "./headphones.png";
 import iMac1 from "./iMac1.png";
 import price from "./price.png";
 
 export {
-  bill,
   card,
-  logo,
   menu,
   close,
-  arrowUp,
   people01,
   people02,
   people03,
@@ -32,7 +25,6 @@ export {
   model4,
   Object_cal,
   phone,
-  headphones,
   iMac1,
   logo6,
   price,
