36ad58ce3a7f1ae56c526a3a12801d17eba129d1
Akshay200228 <sankpla2812@gmail.com>
2023-07-02 17:39:33 +0530

    Add Earth Spline1

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { testimonials } from '../constants/index';
+
+// import styles from '../style';
 import ClientSlider from './ClientSlider';
 import styles from '../style';
 
+
+
 const Testimonials = () => {
+  // destructure testimonials
   const { title, clients } = testimonials;
 
   return (
-    <section
-      id='Testimonials'
-      className={`${styles.paddingX} ${styles.paddingX} md:mt-20`}
-    >
-      <div
-        className='flex flex-col items-center justify-center w-full md:flex-row'
-        data-aos='fade-down'
-        data-aos-delay='500'
+    <section id='Testimonials' className={`${styles.paddingX} mt-20 `}>
+      {/* Testimonials */}
+      <div className="flex flex-row items-center justify-center w-full"
+       data-aos='fade-down' data-aos-delay='500'
       >
-        <h1 className='font-poppins text-center font-semibold text-[36px] sm:text-[36px] text-[26px] text-white ss:leading-[65px] leading-[50px]'>
-          The <span className='text-gradient'>FASTEST</span> Way To Write{" "}
-          <br className='hidden sm:block' /> Copy For Your{" "}
-          <span className='text-gradient'>WEBSITE</span>
+        <h1 className="font-poppins text-center font-semibold md:text-[36px] sm:text-[36px] text-[26px] text-white ss:leading-[65px] leading-[50px]">
+          The <span className='text-gradient'>FASTEST</span> {" "}
+          Way To Write <br className="hidden sm:block" />{" "}
+          Copy For Your <span className='text-gradient'>WEBSITE</span>
         </h1>
       </div>
-
+      
       <div className='container mx-auto' data-aos='fade-right' data-aos-delay='500'>
+        {/* title */}
         <h2 className='title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto'>
           {title}
         </h2>
-        <div className='mx-auto max-w-[920px]'>
+        {/* slider */}
+        <div>
           <ClientSlider clients={clients} />
         </div>
       </div>
