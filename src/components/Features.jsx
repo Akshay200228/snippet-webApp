32270b71a72a443d39ae61b834855aba901483c2
Akshay200228 <sankpla2812@gmail.com>
2023-07-03 20:54:39 +0530

    Fixed Issues

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -9,12 +9,12 @@ const Features = () => {
 
   // destructure feature1
   return (
-    <section id='features' className={`${styles.paddingY} xl:px-0 sm:px-16 px-6 mt-8`}>
+    <section id='features' className={`${styles.paddingY} xl:px-0 sm:px-16 px-6 mt-8 sm:mt-2`}>
       <div className='container mx-auto'>
         {/* Feature1 */}
         <div className='flex flex-col lg:flex-row lg:items-center lg:gap-x-[30px]'>
           {/* text */}
-          <div className='flex-1' data-aos='fade-right' data-aos-offset='400'>
+          <div className='flex-1' data-aos='fade-right' data-aos-offset='300'>
             <div className="text-lg lg:text-[22px] text-dimWhite mb-4 lg:mb-6 uppercase font-normal">{feature1.pretitle}</div>
             <h2 className='mb-4 text-6xl font-bold text-white lg:text-8xl'>{feature1.title}</h2>
             <p className={`${styles.paragraph} mb-4`}>{feature1.subtitle}</p>
@@ -23,7 +23,7 @@ const Features = () => {
             </button>
           </div>
           {/* image */}
-          <div className='flex-1' data-aos='fade-left' data-aos-offset='100'>
+          <div className='flex-1' data-aos='fade-left' data-aos-offset='200'>
             <img src={feature1.image} alt='' className='hidden lg:block' />
           </div>
         </div>
@@ -34,7 +34,7 @@ const Features = () => {
           <div
             className='flex-1 order-2 lg:order-1'
             data-aos='fade-right'
-            data-aos-offset='100'
+            data-aos-offset='200'
           >
             <img className='relative hidden object-cover w-1/2 h-5/6 md:h-5/6 lg:block' src={feature2.image} alt='' />
           </div>
@@ -42,7 +42,7 @@ const Features = () => {
           <div
             className='flex-1 order-1 lg:order-2'
             data-aos='fade-left'
-            data-aos-offset='400'
+            data-aos-offset='300'
           >
             <div className="text-lg lg:text-[22px] text-dimWhite mb-4 lg:mb-6 uppercase font-normal">{feature2.pretitle}</div>
             <h2 className='mb-4 text-6xl font-bold text-white lg:text-8xl'>{feature2.title}</h2>
@@ -54,9 +54,9 @@ const Features = () => {
         </div>
 
         {/* Feature3 */}
-        <div className='flex flex-col lg:flex-row lg:items-center lg:gap-x-[30px]'>
+        <div className='mt-14 sm:mt-4 flex flex-col lg:flex-row lg:items-center lg:gap-x-[30px]'>
           {/* text */}
-          <div className='flex-1' data-aos='fade-right' data-aos-offset='400'>
+          <div className='flex-1' data-aos='fade-right' data-aos-offset='300'>
             <div className="text-lg lg:text-[22px] text-dimWhite mb-4 lg:mb-6 uppercase font-normal">{feature3.pretitle}</div>
             <h2 className='mb-4 text-6xl font-bold text-white lg:text-8xl'>{feature3.title}</h2>
             <p className={`${styles.paragraph} mb-4`}>{feature3.subtitle}</p>
@@ -65,7 +65,7 @@ const Features = () => {
             </button>
           </div>
           {/* image */}
-          <div className='flex-1' data-aos='fade-left' data-aos-offset='100'>
+          <div className='flex-1' data-aos='fade-left' data-aos-offset='200'>
             <img src={feature3.image} alt='' className='hidden lg:block' />
           </div>
         </div>
